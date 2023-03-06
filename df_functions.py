80ffce0eb7fe431f617282e38e7f04c3e998a521
Vhagenbo <98605236+Hagenbo@users.noreply.github.com>
2023-03-06 15:30:26 -0500

    added function for calculating longest streak

--- df_functions.py
+++ df_functions.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import re
+import numpy as np
+from itertools import groupby
 
 
 #Function to read a csv file
@@ -11,7 +13,7 @@ def read_csv_file(csv_file):
 
 #Returns a list of users
 def extract_users(dataframe):
-    return list(dataframe['userID'])
+    return list(dataframe['users'])
 
 
 #Returns a dataframe containing only timestamp, URL or timestamp+sessionID column. 
@@ -43,7 +45,8 @@ def clean_reqlogs(dataframe):
     cleaned_logs = []
 
     for i in range(0, len(request_logs)):
-        sequence = re.sub('[^a-zA-Z]', ' ', request_logs[i])
+        sequence = re.sub('\d', '', request_logs[i])
+        sequence = re.sub(',', ' ', sequence)
         sequence = sequence.lower()
         cleaned_logs.append(sequence)
 
@@ -51,3 +54,36 @@ def clean_reqlogs(dataframe):
     df_cleaned = df_cleaned.drop('URL', axis=1)
 
     return df_cleaned  
+
+#Function to calculate the variance of the values in the different columns
+#Returns the dataframe with a column with variances added
+def calculate_variance(dataframe, list):
+    dataframe['var_reqs'] = dataframe[list].apply(lambda row: row.var(), axis=1)
+    return dataframe
+
+
+#Function to calculate unique requests
+def count_unique_reqs(dataframe, list):
+    dataframe['unique_reqs'] = np.count_nonzero(dataframe[list], axis=1)
+    return dataframe
+
+
+#Function that calculates length of the longest subsequence of consecutive requests DOES NOT WORK
+def longestConsecutive(string):
+    list = re.split(' ', string) 
+    return recursive(list, list[0], 1, 1, 1)
+
+
+#Function that calculates length of the longest subsequence of consecutive requests DOES NOT WORK
+# Need shorter sequences of requests
+def recursive(list, last_word, longest_streak, count, i):
+    if (list[i] == ''  and i < len(list)):
+        return recursive(list, list[i], longest_streak, count, i+1)
+    if (list[i] == last_word):
+        count = count + 1
+    elif (count > longest_streak):
+        longest_streak = count 
+        count = 1     
+    if(i == len(list)):
+        return longest_streak
+    return recursive(list, list[i], longest_streak, count, i+1)
