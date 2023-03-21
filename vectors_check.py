fc8790be76528289cf1d176172ab64e9777bc882
lovix182 <lovisaro@student.chalmers.se>
2023-03-21 10:38:35 +0100

    deleted model_Lovisa. Tried fixing vectorizer functions but not done yet.

--- vectors_check.py
+++ vectors_check.py
@@ -1,6 +1,29 @@
 import pandas as pd
 import re
 import string
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+
+
+"""
+This code contains methods which will clean a string of URLs and perform
+vectorizing operations.
+
+NOTE: the two first methods 'read_csv_file' and 'group_data' is only for testing purposes. will be deleted.
+
+We have two different vectorizers:
+
+    - tf-idf: we vectorize the URLs with the tf-idf vectorizer. The scores
+              will be used for calculating the variance. A high variance means that
+              the data points are very spread out from the mean, and from one another.
+              (Might want to look at standard deviation instead?)
+
+    - count: we vectorize the URLs with the count vectorizer. This counts how many times
+             a url path is used. Url 'words' with high scores indicate they are used a lot.
+
+
+
+"""
+
 
 #the two functions below are just to assemble and simulate a batch of request from one user
 def read_csv_file(csv_file):
@@ -19,16 +42,45 @@ def format_for_url(dataframe):
 
 
 #this function takes "one row" from the df. This will be an instance irl, i.e. one row
-def clean_url_log(dataframe):
+"""def clean_url_log(dataframe):
+   
    pattern = r"\/\d+"
    urls = dataframe['URL'].split(', ')
    for i, url in enumerate(urls):
      urls[i] = re.sub(pattern, "", url)
    dataframe['URL'] = ', '.join(urls)
    return urls
+   
+"""
+def clean_reqlogs(dataframe):
+
+    df_cleaned = dataframe
+    request_logs = df_cleaned['URL']
+
+    cleaned_logs = []
+
+    for i in range(0, len(request_logs)):
+        sequence = re.sub('\d', '', request_logs[i])
+        sequence = re.sub(',', ' ', sequence)
+        sequence = sequence.lower()
+        cleaned_logs.append(sequence)
+
+    df_cleaned ['request_logs'] = cleaned_logs
+    df_cleaned = df_cleaned.drop('URL', axis=1)
+
+    return df_cleaned  
 
 
 
+"""
+Tf-idf vectorizer.
 
+:param timestamp: formatted date to display
+:returns: formatted string
+"""
+def tfidf_vectorizer(dataframe):
+   tfidf_vect = TfidfVectorizer(stop_words = 'english')             #probably don't need stopwords
+   req_log_tf = tfidf_vect.fit_transform(dataframe['request_logs'])
+   return req_log_tf 
 
 
