4d5e23a89109e1cc9d74132ff409a3a347d6a6b6
Vhagenbo <98605236+Hagenbo@users.noreply.github.com>
2023-03-23 17:17:52 -0400

    added longest_consec

--- calculations.py
+++ calculations.py
@@ -1,4 +1,6 @@
 import numpy as np
+from df_functions import clean_reqlogs
+import re
 
 #Function to check the time between the requests in each chunk and calculate the mean
 def calc_avg_timediff(userdata):
@@ -10,3 +12,30 @@ def calc_avg_timediff(userdata):
     avg_ms = np.average(np.diff(timestamps))
     
     return avg_ms/1000
+
+#
+def longest_consec(dataframe):
+    df = dataframe[['userID','URL']].copy()
+    df = df.set_index(['userID']).rename_axis(None)
+    df = df.groupby(level=0).agg(','.join)
+
+    cleaned = clean_reqlogs(df)
+    requests = cleaned['request_logs'].to_list()[0]
+    
+    list = re.split(' ', requests)
+    return recursive_consec(list, list[0], 1, 1, 1)
+    
+
+#Function that calculates length of the longest subsequence of consecutive requests 
+def recursive_consec(list, last_word, longest_streak, count, i):
+    if (list[i] == ''  and i < len(list)):
+        return recursive_consec(list, last_word, longest_streak, count, i+1)
+    if (list[i] == last_word):
+        count = count + 1
+    elif (count > longest_streak):
+        longest_streak = count 
+        count = 1     
+    if(i == len(list)-1):
+        return longest_streak
+    return recursive_consec(list, list[i], longest_streak, count, i+1)
+
